df4fa961e494e4a2cdfa1622397b300cefdc979f
Juan DeCosta <yo@juan-de-costa-rica.com>
2021-06-15 06:34:26 +0000

    first commit

---
+++ truffle-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    networks: {
+      development: {
+        host: "ganache-cli", 
+        port: 8545,
+        network_id: "57771", 
+      },
+    },
+    compilers: {
+      solc: {
+        version: "^0.8.0"
+      }
+    }
+  };
